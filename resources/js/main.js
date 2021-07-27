96237f298b191968d22c55a5e9c51ed9d254ae6d
Dip-Baidya <dipbaidya.cse@gmail.com>
2021-07-27 07:23:35 -0700

    Complete the project

--- resources/js/main.js
+++ resources/js/main.js
@@ -22,3 +22,10 @@ function myFunction() {
     var mixer = mixitup('.container');
 });
 
+function openNav(){
+  document.getElementById("myNav").style.width = "100%";
+}
+function closeNav(){
+  document.getElementById("myNav").style.width = "0%";
+}
+
